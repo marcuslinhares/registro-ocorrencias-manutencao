16f72cc4a79466c9e2c12d3b1ec57113878e41f3
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2026-04-16 11:55:19 -0300

    fix(frontend): embed NEXT_PUBLIC_API_URL at build time and use it in Apollo Client

--- frontend/src/lib/apollo-client.ts
+++ frontend/src/lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/graphql',
   }),
   cache: new InMemoryCache(),
 });
